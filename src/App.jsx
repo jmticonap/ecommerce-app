02f419caebbc7a48e5d74f435637a9ca0c77113c
juancho <jm.ticona.p@gmail.com>
2022-09-30 10:13:42 -0500

    [ADD] navbar v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import JNavbar from './components/JNavbar'
 import {HashRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
--- src/components/JNavbar.jsx
+++ src/components/JNavbar.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 
 const JNavbar = () => {
     return (
-        <Navbar bg="dark" variant="dark">
+        <Navbar bg="primary" variant="dark">
             <Container>
                 <Navbar.Brand as={Link} to='/'>Navbar</Navbar.Brand>
                 <Nav className="me-auto">
